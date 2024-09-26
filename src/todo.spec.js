a973de8f2432cdd1b63292dfca99fa2d85cd6dc9
francisco-kpodotay <135797234+francisco-kpodotay@users.noreply.github.com>
2024-09-26 19:46:56 +0200

    todo.js test created, validate test stil missing

--- src/todo.spec.js
+++ src/todo.spec.js
@@ -1,140 +1,149 @@
-import { jest } from '@jest/globals';
-import { add, format, formatList, list } from './todo.js';
+import { jest } from "@jest/globals";
+import { add, complete, format, formatList, list } from "./todo.js";
 
 function createMockStore(data) {
   return {
     get: jest.fn(() => data),
-    set: jest.fn()
-  }
+    set: jest.fn(),
+  };
 }
 
-describe('format', () => {
-  it('should format a not done todo', () => {
-    const todo = { title: 'todo title', id: 1, done: false };
-    const expected = '1 - [ ] todo title';
+describe("format", () => {
+  it("should format a not done todo", () => {
+    const todo = { title: "todo title", id: 1, done: false };
+    const expected = "1 - [ ] todo title";
 
-    const current = format(todo)
+    const current = format(todo);
 
-    expect(current).toStrictEqual(expected)
-  })
+    expect(current).toStrictEqual(expected);
+  });
 
-  it('should format a done todo', () => {
-    const todo = { title: 'todo title', id: 1, done: true };
-    const expected = '1 - [x] todo title';
+  it("should format a done todo", () => {
+    const todo = { title: "todo title", id: 1, done: true };
+    const expected = "1 - [x] todo title";
 
-    const current = format(todo)
+    const current = format(todo);
 
-    expect(current).toStrictEqual(expected)
+    expect(current).toStrictEqual(expected);
   });
 });
 
-describe('formatList', () => {
-  it('should format a list of todos', () => {
+describe("formatList", () => {
+  it("should format a list of todos", () => {
     const todos = [
-      { title: 'todo title', id: 1, done: true },
-      { title: 'todo title 2', id: 2, done: false }
+      { title: "todo title", id: 1, done: true },
+      { title: "todo title 2", id: 2, done: false },
     ];
-    const expected = [
-      '1 - [x] todo title',
-      '2 - [ ] todo title 2'
-    ];
+    const expected = ["1 - [x] todo title", "2 - [ ] todo title 2"];
 
-    const current = formatList(todos)
+    const current = formatList(todos);
 
-    expect(current).toStrictEqual(expected)
+    expect(current).toStrictEqual(expected);
   }),
-
-    it('should return an empty list, if an empty list is given', () => {
+    it("should return an empty list, if an empty list is given", () => {
       const todos = [];
       const expected = [];
 
-      const current = formatList(todos)
+      const current = formatList(todos);
 
-      expect(current).toStrictEqual(expected)
+      expect(current).toStrictEqual(expected);
     });
 });
 
-describe('list', () => {
-  it('should list the todos', () => {
+describe("list", () => {
+  it("should list the todos", () => {
     const mockStore = createMockStore([
-      { id: 1, title: 'Todo 1', done: false },
-      { id: 2, title: 'Todo 2', done: true }
-    ])
+      { id: 1, title: "Todo 1", done: false },
+      { id: 2, title: "Todo 2", done: true },
+    ]);
     const expected = [
-      { id: 1, title: 'Todo 1', done: false },
-      { id: 2, title: 'Todo 2', done: true }
+      { id: 1, title: "Todo 1", done: false },
+      { id: 2, title: "Todo 2", done: true },
     ];
 
     const current = list(mockStore);
 
     expect(current).toStrictEqual(expected);
-  })
+  });
 
-  it('should return an empty list, if nothing is stored', () => {
-    const mockStore = createMockStore([])
+  it("should return an empty list, if nothing is stored", () => {
+    const mockStore = createMockStore([]);
     const expected = [];
 
     const current = list(mockStore);
 
     expect(current).toStrictEqual(expected);
-  })
-})
+  });
+});
 
-describe('add', () => {
-  it('should add a new todo to an empty store, done false, id is 1', () => {
-    const params = ['New Todo'];
+describe("add", () => {
+  it("should add a new todo to an empty store, done false, id is 1", () => {
+    const params = ["New Todo"];
     const mockStore = createMockStore([]);
     const expected = {
       id: 1,
       done: false,
-      title: 'New Todo'
-    }
+      title: "New Todo",
+    };
 
     const current = add(mockStore, params);
 
     expect(current).toStrictEqual(expected);
-    expect(mockStore.set.mock.calls[0][0])
-      .toStrictEqual([expected]);
+    expect(mockStore.set.mock.calls[0][0]).toStrictEqual([expected]);
   });
 
-  it('should append a new todo to the existing items', () => {
-    const params = ['New Todo'];
-    const stored = [{id: 1, title: 'Todo 1', done: true}];
+  it("should append a new todo to the existing items", () => {
+    const params = ["New Todo"];
+    const stored = [{ id: 1, title: "Todo 1", done: true }];
     const mockStore = createMockStore(stored);
     const expected = {
       id: 2,
       done: false,
-      title: 'New Todo'
-    }
+      title: "New Todo",
+    };
 
     const current = add(mockStore, params);
 
     expect(current).toStrictEqual(expected);
-    expect(mockStore.set.mock.calls[0][0])
-      .toStrictEqual([...stored, expected]);
+    expect(mockStore.set.mock.calls[0][0]).toStrictEqual([...stored, expected]);
   });
 
-  it('should calculate the id by max id + 1, missing ids in a sequence', () => {
-    const params = ['New Todo'];
+  it("should calculate the id by max id + 1, missing ids in a sequence", () => {
+    const params = ["New Todo"];
     const stored = [
-      {id: 2, title: 'Todo 1', done: true},
-      {id: 4, title: 'Todo 1', done: true},
+      { id: 2, title: "Todo 1", done: true },
+      { id: 4, title: "Todo 1", done: true },
     ];
     const mockStore = createMockStore(stored);
     const expected = {
       id: 5,
       done: false,
-      title: 'New Todo'
-    }
-
+      title: "New Todo",
+    };
+    
     const current = add(mockStore, params);
-
+    
     expect(current).toStrictEqual(expected);
-    expect(mockStore.set.mock.calls[0][0])
-      .toStrictEqual([...stored, expected]);
+    expect(mockStore.set.mock.calls[0][0]).toStrictEqual([...stored, expected]);
   });
 });
 
-
-
-
+describe("complete", () => {
+  it("should update a todo done field to true", () => {
+    const params = ["1"];
+    const stored = [
+      { id: 1, title: "Todo 1", done: false },
+    ];
+    const mockStore = createMockStore(stored);
+    const expected = {
+      id: 1,
+      done: true,
+      title: "Todo 1",
+    };
+    
+    const current = complete(mockStore, params);
+    
+    expect(current).toStrictEqual(expected);
+    expect(current.done).toStrictEqual(expected.done);
+  });
+});
